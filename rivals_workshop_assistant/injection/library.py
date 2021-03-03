42eceb3c174090d9d9bb45915aeadd2a579ad18f
Toren Darby <darby@science.ubc.ca>
2021-03-03 15:11:44 -0800

    Make dependency equality checking more precise. Loose checking by name can be handled other than using ==.

--- rivals_workshop_assistant/injection/library.py
+++ rivals_workshop_assistant/injection/library.py
@@ -1,4 +1,4 @@
-from .dependencies import InjectionLibrary
+from . import dependencies
 
 
 def read_injection_library():
@@ -10,5 +10,76 @@ def read_injection_library():
     # Read text to injection_library: list of dependencies
 
 
-def get_injection_library_from_gml(gml: str) -> InjectionLibrary:
-    return []
+def get_injection_library_from_gml(gml: str) -> dependencies.InjectionLibrary:
+    if '#' in gml:
+        name, content = _get_name_and_content(gml)
+        return [dependencies.Define(name=name, content=content)]
+    else:
+        return []
+
+
+def _get_name_and_content(gml: str) -> tuple[str, str]:
+    after_hash_define = gml.split('#define ')[1]
+    return tuple(after_hash_define.split(maxsplit=1))
+
+
+"""
+name_params_version, content = gml.split('\n', 1)
+    name, params, version = _extract_name_params_version(name_params_version)
+
+    docs, gml = _extract_docs_gml(content)
+
+    for dependency_type in (DEPENDENCY_TYPES):
+        if gml.startswith(dependency_type.IDENTIFIER_STRING):
+            return dependency_type(
+                name=name,
+                params=params,
+                version=version,
+                docs=docs,
+                gml=gml,
+                depends=used_dependencies)
+    raise ValueError("Given gml doesn't look like a support dependency.")
+
+
+def _extract_name_params_version(name_params_version_line: str) -> t.Tuple[str, t.List[str], int]:
+    name_params_version = name_params_version_line.replace(Define.IDENTIFIER_STRING, '').strip()
+    has_version = '//' in name_params_version
+    if has_version:
+        name_params, version_str = [section.strip() for section in name_params_version.split('//')]
+        version = int(re.findall(r'\d+', version_str)[0])
+    else:
+        name_params = name_params_version
+        version = 0
+
+    has_params = '(' in name_params
+    if has_params:
+        name, params_str = name_params.split('(')
+        params = params_str.replace(')', '').replace(' ', '').split(',')
+    else:
+        name = name_params
+        params = []
+    return name, params, version
+
+
+def _is_documentation_line(line: str) -> bool:
+    return line.replace(' ', '').replace('\t', '').startswith('//')
+
+
+def _remove_documentation_prefix_from_line(line: str) -> str:
+    return re.sub(r'//\s*', '', line)
+
+
+def _extract_docs_gml(docs_gml: str) -> t.Tuple[str, str]:
+    content_lines = docs_gml.split('\n')
+    doc_lines = []
+    gml_lines = []
+    for line in content_lines:
+        if not gml_lines and _is_documentation_line(line):
+            line = _remove_documentation_prefix_from_line(line)
+            doc_lines.append(line)
+        else:
+            gml_lines.append(line)
+
+    docs = '\n'.join(doc_lines)
+    gml = '\n'.join(gml_lines)
+    return docs, gml"""
--- tests/test_injection_library.py
+++ tests/test_injection_library.py
@@ -1,3 +1,5 @@
+import pytest
+
 from rivals_workshop_assistant.injection.dependencies import Define
 from rivals_workshop_assistant.injection.library import get_injection_library_from_gml
 
@@ -6,13 +8,27 @@ def test_empty():
     library = get_injection_library_from_gml('')
     assert library == []
 
+
 def test_no_dependencies():
     library = get_injection_library_from_gml('nothing helpful here')
     assert library == []
 
-# def test_loads_dependency_minimal():
-#     library = get_injection_library_from_gml("""\
-# #define name
-#     content""")
-#
-#     assert library == [Define(name='name', version=0, content='content')]
+@pytest.mark.parametrize(
+    "content, define",
+    [
+        pytest.param("""\
+#define name
+    content""",
+                     Define(name='name', content='content')),
+        pytest.param("""\
+content
+#define other
+    different content
+""",
+                     Define(name='other', content='different content')),
+    ],
+)
+def test_loads_dependency_minimal(content, define):
+    library = get_injection_library_from_gml(content)
+
+    assert library == [define]
