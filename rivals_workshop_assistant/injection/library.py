3bfdb4605181626dceb4157864e79e259d64af5d
Toren Darby <darby@science.ubc.ca>
2021-03-19 15:19:33 -0700

    Rename config file

--- rivals_workshop_assistant/injection/library.py
+++ rivals_workshop_assistant/injection/library.py
@@ -11,11 +11,12 @@ USER_INJECT_FOLDER = ASSISTANT_FOLDER / Path('user_inject')
 DOTFILE_NAME = '.assistant'
 DOTFILE_PATH = ASSISTANT_FOLDER / DOTFILE_NAME
 
-INJECT_CONFIG_NAME = 'inject_config.ini'
+INJECT_CONFIG_NAME = 'assistant_config.ini'
 INJECT_CONFIG_PATH = ASSISTANT_FOLDER / INJECT_CONFIG_NAME
 
 REPO_OWNER = 'Rivals-Workshop-Community-Projects'
 REPO_NAME = 'injector-library'
+
 
 def read_injection_library(
         root_dir: Path) -> dependency_handling.InjectionLibrary:
