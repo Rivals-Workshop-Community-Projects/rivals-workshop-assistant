96ac2bb265e1c786f45f1a10454986b98975ee8d
Toren Darby <darby@science.ubc.ca>
2021-07-31 22:16:09 -0700

    Fix certain small_sprites false values being read as true

--- rivals_workshop_assistant/character_config_mod.py
+++ rivals_workshop_assistant/character_config_mod.py
@@ -18,6 +18,12 @@ def read(root_dir: Path) -> ConfigParser:
     return config
 
 
+def get_config_truth_value(config_value: str) -> bool:
+    if any([string in config_value.lower() for string in ["0", "false"]]):
+        return False
+    return bool(config_value)
+
+
 def get_has_small_sprites(scripts: List[Script], character_config: ConfigParser):
     in_character_config = character_config.get(
         "general", SMALL_SPRITES_FIELD, fallback=None
@@ -32,6 +38,7 @@ def get_has_small_sprites(scripts: List[Script], character_config: ConfigParser)
     except IndexError:
         init_gml = ""
     match = re.search(pattern=r"small_sprites\s*=\s*(1|true)", string=init_gml)
-    return bool(in_character_config) or bool(match)
+
+    return get_config_truth_value(in_character_config or get_config_truth_value(match))
     # Get character config. Search for 'small_sprites="1"'
     # get init.gml. Search for small_sprites assignment to 1 or true.
