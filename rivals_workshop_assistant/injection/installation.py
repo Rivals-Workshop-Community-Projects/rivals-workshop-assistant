9a6a1aa9f2768738ed0cd572e337e1897516e3ae
Toren Darby <darby@science.ubc.ca>
2022-01-06 12:08:44 -0800

    Update language in anims readme for clarity

--- rivals_workshop_assistant/injection/installation.py
+++ rivals_workshop_assistant/injection/installation.py
@@ -1,9 +1,10 @@
 import rivals_workshop_assistant.assistant_config_mod
 
 ANIMS_FOLDER_README = f"""\
-Put your aseprite files in here.
+Put your character's .aseprite files in here.
 
-If you have `aseprite_path` set in `{rivals_workshop_assistant.assistant_config_mod.PATH}, the assistant will automatically convert them to spritesheets in your sprites folder.
+If you have `aseprite_path` set in `{rivals_workshop_assistant.assistant_config_mod.PATH} to point to your aseprite.exe, 
+then the assistant will automatically convert the .aseprite files in this folder into spritesheets in your sprites folder. 
 They will be saved as `<anim_name>_strip<num_frames>.png`
 This will *overwrite existing sprites with that name.* It is recommended to only change the aseprite file, not the spritesheets, to avoid losing changes.
 """
