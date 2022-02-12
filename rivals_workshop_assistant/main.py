b0466ec85fb7c6c32d6baafe1db4d6c89d7dc1d1
Toren Darby <darby@science.ubc.ca>
2022-02-12 15:16:29 -0800

    Bump version

--- rivals_workshop_assistant/main.py
+++ rivals_workshop_assistant/main.py
@@ -56,7 +56,7 @@ from rivals_workshop_assistant.injection import (
 from rivals_workshop_assistant.code_generation import handle_codegen
 from rivals_workshop_assistant.warning_handling import handle_warning
 
-__version__ = "1.2.19"
+__version__ = "1.2.20"
 
 
 class Mode(Enum):
