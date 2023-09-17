bd8c769f9e0cb1064fcbfe292c83149e52194148
Toren Darby <torendarby@gmail.com>
2023-09-17 14:33:05 -0700

    chore: bump version

--- rivals_workshop_assistant/main.py
+++ rivals_workshop_assistant/main.py
@@ -44,7 +44,7 @@ from rivals_workshop_assistant.script_handling.injection import (
     freshen_scripts_that_have_modified_dependencies,
 )
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 
 def run_as_file():
